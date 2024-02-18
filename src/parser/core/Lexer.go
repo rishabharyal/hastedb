3893c940485e517576665e2ae500d8d2a0a619de
Rishabh Aryal <rishabharyal@protonmail.com>
2024-02-18 18:13:02 +0545

    added tokenizer basic logic

--- src/parser/core/Lexer.go
+++ src/parser/core/Lexer.go
@@ -1,6 +1,38 @@
 package core
 
+import (
+    "strings"
+)
+
 type Token struct {
     Token string
     Type string
+}
+
+type Lexer struct {
+    Source string
+    Position int
+    CurrentChar string
+    Tokens []Token
+    chars []string
+}
+
+func NewLexer(source string) *Lexer {
+    l := &Lexer{Source: source, Position: 0, CurrentChar: string(source[0]), Tokens: []Token{}}
+    return l
+}
+
+// sample query: SELECT * FROM table WHERE id = 1
+func (l *Lexer) Tokenize() {
+    l.chars = strings.Split(l.Source, "")
+    l.Position = 0
+
+    if len(l.chars) == 0 {
+        return
+    }
+
+    for l.Position < len(l.chars) {
+
+    }
+
 }
