360f66bd6c5de74fae5de9e8e884ae69ad23d6ce
Rishabh Aryal <rishabharyal@protonmail.com>
2024-02-20 22:11:03 +0545

    handled closing part of the string

--- src/parser/core/Lexer.go
+++ src/parser/core/Lexer.go
@@ -15,6 +15,7 @@ type Lexer struct {
     CurrentChar string
     Tokens []Token
     chars []string
+    isString bool
 }
 
 func NewLexer(source string) *Lexer {
@@ -32,7 +33,22 @@ func (l *Lexer) Tokenize() {
     }
 
     for l.Position < len(l.chars) {
-
+        l.CurrentChar = l.chars[l.Position]
+        // if the instance in a string, any character is candidate for a string literal
+        if l.isString {
+            if l.CurrentChar != "\"" {
+                // Add the current character to the chars array
+                l.chars = append(l.chars, l.CurrentChar)
+                l.Position++
+                continue
+            }
+        }
+        // Sinc this is already a string, and we also got a closing double quote, we can add the string to the tokens array
+        l.Tokens = append(l.Tokens, Token{Token: strings.Join(l.chars, ""), Type: "string"})
+        l.chars = []string{}
+        l.isString = false
+        l.Position++
+        continue
     }
 
 }
