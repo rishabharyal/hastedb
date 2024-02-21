1d0fb7d08d7b1387f611bc7bf188e4124f246057
Rishabh Aryal <rishabharyal@protonmail.com>
2024-02-21 20:07:36 +0545

    added code for token types and increment the tokens

--- src/parser/core/Lexer.go
+++ src/parser/core/Lexer.go
@@ -4,6 +4,44 @@ import (
     "strings"
 )
 
+
+var TOKEN_TYPES = map[string]string{
+    "SELECT": "select",
+    "FROM": "from",
+    "WHERE": "where",
+    "AND": "and",
+    "OR": "or",
+    "NOT": "not",
+    "IN": "in",
+    "LIKE": "like",
+    "BETWEEN": "between",
+    "IS": "is",
+    "NULL": "null",
+    "ORDER": "order",
+    "BY": "by",
+    "ASC": "asc",
+    "DESC": "desc",
+    "LIMIT": "limit",
+    "OFFSET": "offset",
+    "INSERT": "insert",
+    "INTO": "into",
+    "VALUES": "values",
+    "UPDATE": "update",
+    "SET": "set",
+    "DELETE": "delete",
+    "CREATE": "create",
+    "TABLE": "table",
+    "DROP": "drop",
+    "ALTER": "alter",
+    "ADD": "add",
+    "PRIMARY": "primary",
+    "KEY": "key",
+    "FOREIGN": "foreign",
+    "REFERENCES": "references",
+    "CONSTRAINT": "constraint",
+    "UNIQUE": "unique",
+}
+
 type Token struct {
     Token string
     Type string
@@ -42,13 +80,35 @@ func (l *Lexer) Tokenize() {
                 l.Position++
                 continue
             }
+        } else {
+            // Sinc this is already a string, and we also got a closing double quote, we can add the string to the tokens array
+            l.Tokens = append(l.Tokens, Token{Token: strings.Join(l.chars, ""), Type: "string"})
+            l.chars = []string{}
+            l.isString = false
+            l.Position++
+            continue
         }
-        // Sinc this is already a string, and we also got a closing double quote, we can add the string to the tokens array
-        l.Tokens = append(l.Tokens, Token{Token: strings.Join(l.chars, ""), Type: "string"})
-        l.chars = []string{}
-        l.isString = false
-        l.Position++
-        continue
-    }
+        if l.CurrentChar == "\"" {
+            l.isString = true
+            l.Position++
+            continue
+        }
 
+        // if the current character is a space, we need to save current token and reset the chars array
+        if l.CurrentChar == " " {
+            l.HandleToken()
+            l.chars = []string{}
+            l.Position++
+            continue
+        }
+
+        l.chars = append(l.chars, l.CurrentChar)
+        l.Position++
+
+    }
+    
+
+}
+
+func (l *Lexer) HandleToken() {
 }
